99b763eb5fe9a3baed1f313f9886186c14b5c670
coker2019 <olabisi2k11@gmail.com>
2023-10-21 10:07:41 +0100

    Add fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,4 +25,22 @@ RSpec.describe Solver do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
+  describe ‘#fizzbuzz’ do
+    it ‘returns “fizz” when given a multiple of 3’ do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq(‘fizz’)
+    end
+    it ‘returns “buzz” when given a multiple of 5’ do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq(‘buzz’)
+    end
+    it ‘returns “fizzbuzz” when given a multiple of both 3 and 5’ do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq(‘fizzbuzz’)
+    end
+    it ‘returns the number as a string for other cases’ do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq(‘7’)
+    end
+  end
 end
